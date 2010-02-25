1f056fd0e345e18f780fa132c6a879ef65ce5991
Nahuel Chaves <nahuel.chaves@gmail.com>
2010-02-25 21:05:22 +0000

    Ajax

--- app/controllers/tb_plantillas_controller.rb
+++ app/controllers/tb_plantillas_controller.rb
@@ -7,9 +7,30 @@ class TbPlantillasController < ApplicationController
 	@tb_plantilla = TbPlantilla.new
   end
 
+  def create
+    @tb_plantilla = TbPlantilla.create(params[:tb_plantilla])
+    #flash[:success] = "Plantilla creada correctamente."
+    respond_to do |format|
+      format.html { redirect_to tb_plantillas_path }
+      format.js {render :layout => false}
+    end
+  end
+  
   def edit
 	@tb_plantilla = TbPlantilla.find(params[:id])
   end
+
+  def update
+    @tb_plantilla = TbPlantilla.find(params[:id])
+     if @tb_plantilla.update_attributes(params[:tb_plantilla])
+      flash[:success] = "Plantilla actualizada correctamente."
+      redirect_to tb_plantillas_path
+     else
+      flash[:error] = "Hubo un error"
+      render :action => "edit"
+    end
+  end
+
   def destroy
     @tb_plantilla = TbPlantilla.find(params[:id])
     if @tb_plantilla.destroy
--- app/models/tb_plantilla.rb
+++ app/models/tb_plantilla.rb
@@ -1,5 +1,7 @@
 class TbPlantilla < ActiveRecord::Base
   set_primary_key "id_plantilla"
+  validates_presence_of :pl_codigo
+  validates_uniqueness_of :pl_codigo
   def self.per_page
     10
   end
